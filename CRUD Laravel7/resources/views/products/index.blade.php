771ad50db78de1e27db434eafb89263a29044da4
Pyae Phyo Aung <pyaephyoaungtcl.98@gmail.com>
2020-04-17 10:55:36 +0700

    CRUD Laravel7

---
+++ CRUD Laravel7/resources/views/products/index.blade.php
@@ -0,0 +1,48 @@
+@extends('products.layout')
+@section('content')
+
+<div class="row">
+    <div class="row-lg-12  margin-tb">
+        <div class="pull-left">
+            <h2>Laravel 7 CRUD Example from scratch</h2>
+        </div>
+        <div class="pull-right">
+            <a href="{{route('products.create')}}" class="btn btn-success">Create New Product</a>
+        </div>
+    </div>
+</div>
+@if($message = Session::get('success'))
+<div class="alert alert-success">
+    <p>{{$message}}</p>
+</div>
+@endif
+
+<table class="table table-bordered">
+    <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th>Detail</th>
+        <th width="280px">Action</th>
+    </tr>
+    @foreach($products as $product)
+    <tr>
+        <td>{{$product->id}}</td>
+        <td>{{ $product->name}}</td>
+        <td>{{ $product->detail}}</td>
+        <td>
+            <form action="{{route('products.destroy',$product->id)}}" method="POST">
+                <a href="{{route('products.show',$product->id)}}" class="btn btn-info">Show</a>
+                <a href="{{route('products.edit',$product->id)}}" class="btn btn-primary">Edit</a>
+                @csrf
+                @method('DELETE')
+
+                <button type="submit" class="btn btn-danger">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+{!! $products->links() !!}
+
+@endsection
